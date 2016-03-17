4eb79fade50226467d5d91cbf58dc60dac50febf
Dang Mai <contact@dangmai.net>
2016-03-17 15:48:13 -0400

    Optimize deployment steps a bit better

--- deploy.sh
+++ deploy.sh
@@ -28,11 +28,10 @@ tar xvfz cf.tgz
 # Restart the container
 OLD_CONTAINER_NAME=`./cf ic ps | grep -oE '[^ ]+$' | grep $CONTAINER_NAME.* | cat`
 NEW_CONTAINER_NAME="$CONTAINER_NAME.`date +%s`"
-if [ "$OLD_CONTAINER_NAME" != "" ]; then
-  ./cf ic ip unbind $PUBLIC_IP $OLD_CONTAINER_NAME
-  ./cf ic stop $OLD_CONTAINER_NAME
-  ./cf ic rm $OLD_CONTAINER_NAME
-fi
 ./cf ic run -p 80 -m $BLUEMIX_MEMORY --name $NEW_CONTAINER_NAME registry.ng.bluemix.net/$REPO
 sleep 30s  # sometimes we can't bind the IP to a container in a very early stage of building
+if [ "$OLD_CONTAINER_NAME" != "" ]; then
+  ./cf ic ip unbind $PUBLIC_IP $OLD_CONTAINER_NAME
+  ./cf ic rm -f $OLD_CONTAINER_NAME
+fi
 ./cf ic ip bind $PUBLIC_IP $NEW_CONTAINER_NAME
